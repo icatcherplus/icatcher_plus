ccb2d0b38f2f092ee489dbf11f83db6a522547bd
GalRaz <galraz@gmail.com>
2023-05-25 15:27:30 -0700

    Update illegal transitions function in parsers.py
    this is to accommodate illegal transitions with negative values (-1,-2), which the current function isn't equipped to handle

--- src/icatcher/parsers.py
+++ src/icatcher/parsers.py
@@ -1,3 +1,5 @@
+import re
+
 def parse_illegal_transitions_file(path, skip_header=True):
     illegal_transitions = []
     corrected_transitions = []
@@ -8,14 +10,19 @@ def parse_illegal_transitions_file(path, skip_header=True):
         if skip_header:
             rows = rows[1:]
         for row in rows:
-            row = row.split(",")
-            row = [x.strip() for x in row]
+            row = row.strip().split(",")
+            row = [x.replace('"', '') for x in row]
             if len(row) != 2:
                 raise ValueError("Illegal transitions file needs to have exactly two columns.")
             ilegal, corrected = row
             try:
-                illegal_transitions.append([int(x) for x in ilegal])
-                corrected_transitions.append([int(x) for x in corrected])
+                # Separate by each digit or negative number, if "-" appears, it should be with the next digit
+                ilegal_transitions = [int(x) for x in re.findall(r'-?\d', ilegal)]
+                illegal_transitions.append(ilegal_transitions)
+
+                # Treat the entire corrected transition as a single entity
+                corrected_transition = [int(x) for x in re.findall(r'-?\d', corrected)]
+                corrected_transitions.append(corrected_transition)
             except ValueError:
                 raise ValueError("Illegal transitions file needs to have only integers.")
     return illegal_transitions, corrected_transitions
