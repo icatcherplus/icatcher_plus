ea686fdc49eeeb95940a6f0598de4095ce9bafc4
Deirdre Kelliher <dmkelliher@gmail.com>
2023-06-19 12:44:28 -0400

    Collapsing instructions to increase audit screen real estate

--- interface/frontend/results_tool/src/App.jsx
+++ interface/frontend/results_tool/src/App.jsx
@@ -1,4 +1,8 @@
+import { 
+  Drawer
+} from '@mui/material';
 import styles from './App.module.css';
+import Instructions from './components/Instructions';
 import FileModal from './components/FileModal';
 import Snacks from './components/common/Snacks';
 import VideoFrame from './components/video/VideoFrame';
@@ -8,13 +12,7 @@ function App() {
   return (
     <div className={styles.app}>
       <div className={styles.mainpage}>
-        <div className={styles.instructionContainer}>
-          <h4 className={styles.instructionsHeader}>Welcome to iCatcher+</h4>
-          <p className={styles.instructions}>Click the video or press SPACE to start play</p>
-          <p className={styles.instructions}>Use the 'r' key to play the video in reverse</p>
-          <p className={styles.instructions}>Use the left and right arrow keys to page through the video frames</p>
-          <p className={styles.instructions}>Jump to a specific frame using the input box above the video</p>
-        </div>
+        <Instructions />
         <VideoFrame />
       </div>
       <FileModal />
