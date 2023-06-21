3d0a413f5c5738feaa343559424a71e48f71ab8a
samwinebrake <85908068+samwinebrake@users.noreply.github.com>
2023-06-20 21:26:57 -0400

    Update to non-hashed zip directory

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,14 +5,14 @@ from batch_face import RetinaFace
 
 # download models that will be tested. these will be cached
 GOODBOY = pooch.create(path=pooch.os_cache("icatcher_plus"),
-                       base_url="https://osf.io/ycju8/download",
-                       version=version,
-                       version_dev="main",
-                       env="ICATCHER_DATA_DIR",
-                       registry={"zip_content.txt": "d81bfb5a183edea6dc74f7f342d516a9843865570b9ecfbf481209ec5114110a",
-                                 "icatcher+_models.zip": "d78385b3a08f3d55ce75249142d15549e4c5552d5e1231cad3b69063bb778ce9"},
-                       urls={"zip_content.txt": "https://osf.io/v4w53/download",
-                             "icatcher+_models.zip": "https://osf.io/ycju8/download"})
+                           base_url="https://osf.io/h7svp/download",
+                           version=version,
+                           version_dev="main",
+                           env="ICATCHER_DATA_DIR",
+                           registry={"zip_content.txt": None,
+                                     "icatcher+_models.zip": None},
+                           urls={"zip_content.txt":"https://osf.io/v4w53/download",
+                                 "icatcher+_models.zip":"https://osf.io/h7svp/download"})
 
 file_paths = GOODBOY.fetch("icatcher+_models.zip",
                            processor=pooch.Unzip(),
