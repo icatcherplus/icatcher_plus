705e7088b378f1451af0f73855c5e3cb228c0976
samwinebrake <85908068+samwinebrake@users.noreply.github.com>
2023-08-21 15:28:13 -0400

    simplified preload_models.py

--- docker/preload_models.py
+++ docker/preload_models.py
@@ -1,36 +1,9 @@
-import sys
-import pooch
-
-def load_models(version):
-    GOODBOY = pooch.create(
-        path=pooch.os_cache("icatcher_plus"),
-        base_url="https://osf.io/h7svp/download",
-        version=version,
-        version_dev="main",
-        env="ICATCHER_DATA_DIR",
-        registry={"zip_content.txt": None, "icatcher+_models.zip": None},
-        urls={
-            "zip_content.txt": "https://osf.io/v4w53/download",
-            "icatcher+_models.zip": "https://osf.io/h7svp/download",
-        },
-    )
-    
-    file_paths = GOODBOY.fetch(
-        "icatcher+_models.zip", processor=pooch.Unzip()
-    )
-
-
-def strip_version(version_output):
-    version = version_output.split("==")[1]
-    return version
+from src.icatcher.cli import load_models
 
 
 def main():
-    version_output = sys.argv[1]
-    version = strip_version(version_output)
-    load_models(version)
+    load_models(opt=None, download_only=True)
 
 
 if __name__ == '__main__':
     main()
-  
