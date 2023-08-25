c8aec7a0069ced30a5a22a0a3047ee3905a75d15
Yotam Erel <erelyotam@gmail.com>
2023-08-25 12:51:34 +0900

    resolves #62

--- src/icatcher/__init__.py
+++ src/icatcher/__init__.py
@@ -1,5 +1,5 @@
 ### define version
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 version = __version__
 ### define classes
 classes = {"noface": -2, "nobabyface": -1, "away": 0, "left": 1, "right": 2}
