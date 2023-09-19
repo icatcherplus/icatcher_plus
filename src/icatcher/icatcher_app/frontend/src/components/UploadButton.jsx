1e97f3885a61d10a0ed58efb82a88031a4492937
Deirdre Kelliher <dmkelliher@gmail.com>
2023-09-19 19:43:17 -0400

    adding number disply for file upload button

--- src/icatcher/icatcher_app/frontend/src/components/UploadButton.jsx
+++ src/icatcher/icatcher_app/frontend/src/components/UploadButton.jsx
@@ -1,35 +1,39 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 function UploadButton (props) {
  
   const { handleInput } = props;
   const hiddenInputElement = useRef(null);
+  const [ inputSize, setInputSize ] = useState(null);
   
   const handleClick = (event) => {
     hiddenInputElement.current.click();
   };
 
   const handleChange = (event) => {
+    event.target.files ? setInputSize(event.target.files.length) : setInputSize(null)
     handleInput(event);
-    console.log("EEE", hiddenInputElement.current)
-    console.log("SSS", event)
-
   };
 return (
-    <React.Fragment>
+    <div style={{display:'flex', gap: '10px'}}>
       <button className="button-upload" onClick={handleClick}>
         Upload output directory
       </button>
       <input 
-          type="file" 
-          id="fileInput" 
-          onChange={handleChange}
-          ref={hiddenInputElement}
-          webkitdirectory=""
-          style={{display: 'none'}}
-        />
-        <div />
-    </React.Fragment>
+        type="file" 
+        id="fileInput" 
+        onChange={handleChange}
+        ref={hiddenInputElement}
+        webkitdirectory=""
+        style={{display: 'none'}}
+      />
+      <div>
+        { inputSize == null 
+          ? ''
+          : `${inputSize} files`
+        }
+      </div>
+    </div>
   );
 }
 
