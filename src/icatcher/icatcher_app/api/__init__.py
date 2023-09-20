3d114169c49c9db7be5ec29c178ee7dcb6ad6ec4
deirdre-k <deirdre-k@users.noreply.github.com>
2023-09-20 18:13:30 +0000

    Auto-format code with black

--- src/icatcher/icatcher_app/api/__init__.py
+++ src/icatcher/icatcher_app/api/__init__.py
@@ -3,6 +3,7 @@ from .api import run_app as run_icatcher_app
 
 default_port = 5001
 
+
 def open_app():
     webbrowser.open(f"http://localhost:{default_port}")
     run_icatcher_app(port=default_port)
